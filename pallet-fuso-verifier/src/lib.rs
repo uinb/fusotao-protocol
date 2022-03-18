a88b1cb879aa75447c6c488722063266ad21869f
Stewart Ye <16131485+StewartYe@users.noreply.github.com>
2022-03-18 16:55:43 +0800

    update (#42)

--- pallet-fuso-verifier/src/lib.rs
+++ pallet-fuso-verifier/src/lib.rs
@@ -588,7 +588,8 @@ pub mod pallet {
             let dominator = Dominators::<T>::try_get(&dominator_id)
                 .map_err(|_| Error::<T>::DominatorNotFound)?;
             if dominator.status == DOMINATOR_EVICTED {
-                Reserves::<T>::try_mutate_exists(
+				 ensure!(1 == 2, Error::<T>::DominatorEvicted);
+             /*   Reserves::<T>::try_mutate_exists(
                     &(RESERVE_FOR_AUTHORIZING_STASH, fund_owner.clone(), token_id),
                     &dominator_id,
                     |ov| -> DispatchResult {
@@ -613,7 +614,7 @@ pub mod pallet {
                     amount,
                     &dominator_id,
                 )?;
-                Receipts::<T>::remove(dominator_id.clone(), &fund_owner);
+                Receipts::<T>::remove(dominator_id.clone(), &fund_owner);*/
             } else {
                 ensure!(
                     !Receipts::<T>::contains_key(&dominator_id, &fund_owner),
