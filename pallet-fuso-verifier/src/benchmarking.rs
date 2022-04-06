41ca78088a5ad5a3207be61780ba863abd1f3c37
Ben <codingqi@gmail.com>
2022-04-06 16:39:15 +0800

    fusotao verifier benchmarking

--- pallet-fuso-verifier/src/benchmarking.rs
+++ pallet-fuso-verifier/src/benchmarking.rs
@@ -3,6 +3,7 @@ use crate::Pallet as Verifier;
 pub use frame_benchmarking::{account, benchmarks};
 use frame_system::RawOrigin;
 use sp_runtime::traits::StaticLookup;
+
 const SEED: u32 = 0;
 
 pub type BalanceOf<T> = <T as pallet_balances::Config>::Balance;
@@ -20,8 +21,18 @@ benchmarks! {
 
     register {
         frame_system::Pallet::<T>::set_block_number(30000.into());
-        let caller: T::AccountId = account("caller", 0, SEED);
-    } :_(RawOrigin::Signed(caller), b"alice".to_vec())
+        let alice: T::AccountId = account("alice", 0, SEED);
+    } :_(RawOrigin::Signed(alice), b"cool".to_vec())
+
+    evict {
+        frame_system::Pallet::<T>::set_block_number(30000.into());
+        let alice: T::AccountId = account("alice", 0, SEED);
+        let _ = Verifier::<T>::register(
+            <T as frame_system::Config>::Origin::from(RawOrigin::Signed(alice.clone())),
+            b"cool".to_vec()
+        )?;
+        let dominator = T::Lookup::unlookup(alice.clone());
+    } :_(RawOrigin::Signed(alice), dominator)
 
     stake {
         frame_system::Pallet::<T>::set_block_number(30000.into());
@@ -83,10 +94,38 @@ benchmarks! {
             br#"usdt.testnet"#.to_vec(),
         )?;
         pallet_fuso_token::Pallet::<T>::do_mint(
-            ,
+            1u32.into(),
             &ferdie,
             10000000.into(),
             None
         )?;
     } :_(RawOrigin::Signed(ferdie), dominator, 1.into(), 500000000000.into())
+
+    revoke {
+        frame_system::Pallet::<T>::set_block_number(2000.into());
+        let ferdie: T::AccountId =  account("ferdie", 0, SEED);
+        let alice:  T::AccountId =  account("alice", 0, SEED);
+        let _ = Verifier::<T>::register(
+            <T as frame_system::Config>::Origin::from(RawOrigin::Signed(alice.clone())),
+            b"cool".to_vec()
+        )?;
+        let dominator = T::Lookup::unlookup(alice);
+        pallet_fuso_token::Pallet::<T>::issue(
+            <T as frame_system::Config>::Origin::from(RawOrigin::Signed(ferdie.clone())),
+            6,
+            true,
+            br#"USDT"#.to_vec(),
+            br#"usdt.testnet"#.to_vec(),
+        )?;
+        pallet_fuso_token::Pallet::<T>::do_mint(
+            1u32.into(),
+            &ferdie,
+            10000000.into(),
+            None
+        )?;
+        Verifier::<T>::authorize(
+             <T as frame_system::Config>::Origin::from(RawOrigin::Signed(ferdie.clone())), dominator.clone(), 1.into(), 500000000000.into()
+        )?;
+    } :_(RawOrigin::Signed(ferdie), dominator, 1.into(), 500000000000.into())
+
 }
