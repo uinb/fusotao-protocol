d8da765efe43987b048426ccb1b9a213c5d25114
Cyberaurora <22611640+cyberaurora@users.noreply.github.com>
2022-09-29 22:56:58 +0800

    code stash

--- pallet-fuso-dao/src/lib.rs
+++ pallet-fuso-dao/src/lib.rs
@@ -42,26 +42,83 @@ pub mod pallet {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
         type Asset: Token<Self::AccountId>;
+
+        type Call: Parameter
+            + Dispatchable<Origin = Self::Origin, PostInfo = PostDispatchInfo>
+            + GetDispatchInfo
+            + From<frame_system::Call<Self>>;
     }
 
+    // TODO
+    pub type ProposalIndex = u32;
+
     #[derive(Encode, Decode, Clone, PartialEq, Eq, TypeInfo, Debug)]
-    pub struct DAO<AccountId, TokenId, Balance> {
+    pub struct DAO<AccountId, TokenId, Balance, BlockNumber> {
         pub name: Vec<u8>,
         pub logo: Vec<u8>,
         pub lang: Vec<u8>,
+        pub gov_token: TokenId,
+        pub mintable: bool,
+        pub mint_by: TokenId,
         pub treasury: AccountId,
         pub originator: AccountId,
         pub max_members: u32,
-        pub governance_token: TokenId,
-        pub rule: DAORule,
-        pub entry_threshold: Balance,
+        pub rule: DAORule<BlockNumber>,
+        pub valid: bool,
+        pub proposal_index: ProposalIndex,
     }
 
     #[derive(Encode, Decode, Clone, PartialEq, Eq, TypeInfo, Debug)]
-    pub enum DAORule {
+    pub enum Proposal<AccountId, BlockNumber, Call, TokenId, Balance> {
+        OnchainNeedCharge {
+            pub issuer: AccountId,
+            pub expire_at: BlockNumber,
+            pub status: u8,
+            pub url: Vec<u8>,
+            pub call: Call,
+            pub token_id: TokenId,
+            pub amount: Balance,
+            pub voting: (Balance, Balance, Balance),
+        },
+        OffchainNeedCharge {
+            pub issuer: AccountId,
+            pub expire_at: BlockNumber,
+            pub status: u8,
+            pub url: Vec<u8>,
+            pub token_id: TokenId,
+            pub amount: Balance,
+            pub voting: (Balance, Balance, Balance),
+        },
+        Onchain {
+            pub issuer: AccountId,
+            pub expire_at: BlockNumber,
+            pub status: u8,
+            pub url: Vec<u8>,
+            pub call: Call,
+            pub voting: (Balance, Balance, Balance),
+        },
+        Offchain {
+            pub issuer: AccountId,
+            pub expire_at: BlockNumber,
+            pub status: u8,
+            pub url: Vec<u8>,
+            pub voting: (Balance, Balance, Balance),
+        },
+    }
+
+    #[derive(Encode, Decode, Clone, PartialEq, Eq, TypeInfo, Debug)]
+    pub enum DAORule<Term> {
         Unanimity,
         Majority(Percent),
-        Delegation(u16),
+    }
+
+    #[derive(Encode, Decode, Clone, PartialEq, Eq, TypeInfo, Debug)]
+    pub enum GovernanceToken<TokenId, Balance> {
+        Existing(TokenId),
+        New {
+            token_symbol: Vec<u8>,
+            mint_by: TokenId,
+        },
     }
 
     #[pallet::storage]
@@ -70,13 +127,13 @@ pub mod pallet {
         _,
         Blake2_128Concat,
         T::AccountId,
-        DAO<T::AccountId, TokenId<T>, Balance<T>>,
+        DAO<T::AccountId, TokenId<T>, Balance<T>, T::BlockNumber>,
         OptionQuery,
     >;
 
     #[pallet::storage]
-    #[pallet::getter(fn org_members)]
-    pub type OrgMembers<T: Config> = StorageDoubleMap<
+    #[pallet::getter(fn members)]
+    pub type Members<T: Config> = StorageDoubleMap<
         _,
         Blake2_128Concat,
         T::AccountId,
@@ -86,12 +143,27 @@ pub mod pallet {
         OptionQuery,
     >;
 
+    #[pallet::storage]
+    #[pallet::getter(fn proposals)]
+    pub type Proposals<T: Config> = StorageDoubleMap<
+        _,
+        Blake2_128Concat,
+        T::AccountId,
+        Blake2_128Concat,
+        ProposalIndex,
+        Proposal<T::AccountId, T::BlockNumber, T::Call, TokenId<T>, Balance<T>>,
+        OptionQuery,
+    >;
+
     #[pallet::event]
     #[pallet::generate_deposit(pub (super) fn deposit_event)]
     pub enum Event<T: Config> {}
 
     #[pallet::error]
-    pub enum Error<T> {}
+    pub enum Error<T> {
+        DaoNameAlreadyExisted,
+        AccountImplyError,
+    }
 
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
@@ -102,5 +174,95 @@ pub mod pallet {
     pub struct Pallet<T>(_);
 
     #[pallet::call]
-    impl<T: Config> Pallet<T> {}
+    impl<T: Config> Pallet<T> {
+        // TODO
+        #[pallet::weight(0)]
+        pub fn proposal(
+            origin: OriginFor<T>,
+            org: T::AccountId,
+            proposal: Box<T::Call>,
+            expire_at: T::BlockNumber,
+        ) -> DispatchResultWithPostInfo {
+            Ok(().into())
+        }
+
+        #[pallet::weight(0)]
+        pub fn approve(
+            origin: OriginFor<T>,
+            org: T::AccountId,
+            index: ProposalIndex,
+        ) -> DispatchResultWithPostInfo {
+            Ok(().into())
+        }
+
+        #[pallet::weight(0)]
+        pub fn reject(
+            origin: OriginFor<T>,
+            org: T::AccountId,
+            index: ProposalIndex,
+        ) -> DispatchResultWithPostInfo {
+            Ok(().into())
+        }
+
+        /// invoke by the originators
+        #[pallet::weight(1_000_000_000_000_000_000)]
+        pub fn create(
+            origin: OriginFor<T>,
+            name: Vec<u8>,
+            logo: Vec<u8>,
+            lang: Vec<u8>,
+            max_members: u32,
+            gov_token: GovernanceToken<TokenId<T>, Balance<T>>,
+            rule: DAORule<T::BlockNumber>,
+            entry_threshold: Balance<T>,
+        ) -> DispatchResultWithPostInfo {
+            let originator = ensure_signed(origin)?;
+            let treasury_account = Self::imply_account(name.clone(), ProposalIndex::default())?;
+            ensure!(
+                !Orgs::<T>::contains_key(&treasury_account),
+                Error::<T>::DaoNameAlreadyExisted
+            );
+            // let gov_token = match gov_token {
+            //     Existing(token_id) => token_id,
+            //     New {
+            //         token_symbol,
+            //         mint_by,
+            //     } => {
+
+            //     }
+            // }
+            // TODO params check
+            Orgs::<T>::insert(
+                treasury_account,
+                DAO {
+                    name,
+                    logo,
+                    lang,
+                    treasury: treasury_account,
+                    originator,
+                    max_members,
+                    rule,
+                    valid: false,
+                    proposal_index: ProposalIndex::default(),
+                },
+            );
+            Ok(().into())
+        }
+    }
+
+    impl<T: Config> Pallet<T> {
+        pub fn imply_account(
+            name: Vec<u8>,
+            index: ProposalIndex,
+        ) -> Result<T::AccountId, Error<T>> {
+            let deterministic =
+                (b"#_fuso_dao_#", name, index).using_encoded(sp_io::hashing::blake2_256);
+            Decode::decode(&mut TrailingZeroInput::new(deterministic.as_ref()))
+                .map_err(|_| Error::<T>::AccountImplyError)
+        }
+
+        fn join(joinee: T::AccountId, dao: T::AccountId) -> DispatchResult<()> {
+            Ok(())
+        }
+    }
 }
