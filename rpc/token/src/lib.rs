6bb6e58c125de5257510aa10b5b6ea501a144c97
Kachinsky <22611640+cyberaurora@users.noreply.github.com>
2023-03-01 16:40:05 +0800

    rename args

--- rpc/token/src/lib.rs
+++ rpc/token/src/lib.rs
@@ -4,8 +4,8 @@ use sp_rpc::number::NumberOrHex;
 #[rpc(client, server)]
 pub trait TokenApi<BlockHash, Account, Balance> {
     #[method(name = "token_freeBalance")]
-    fn free_balance(&self, who: AccountId, at: Option<BlockHash>) -> RpcResult<Balance>;
+    fn free_balance(&self, who: Account, at: Option<BlockHash>) -> RpcResult<Balance>;
 
     #[method(name = "token_totalBalance")]
-    fn total_balance(&self, who: AccountId, at: Option<BlockHash>) -> RpcResult<Balance>;
+    fn total_balance(&self, who: Account, at: Option<BlockHash>) -> RpcResult<Balance>;
 }
