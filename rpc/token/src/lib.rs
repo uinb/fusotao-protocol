38cbbd24e30ef6d1f4ff53f69c526274ad65b102
Kachinsky <22611640+cyberaurora@users.noreply.github.com>
2023-03-01 16:24:29 +0800

    refactor project structure

---
+++ rpc/token/src/lib.rs
@@ -0,0 +1,11 @@
+use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+use sp_rpc::number::NumberOrHex;
+
+#[rpc(client, server)]
+pub trait TokenApi<BlockHash, Account, Balance> {
+    #[method(name = "token_freeBalance")]
+    fn free_balance(&self, who: AccountId, at: Option<BlockHash>) -> RpcResult<Balance>;
+
+    #[method(name = "token_totalBalance")]
+    fn total_balance(&self, who: AccountId, at: Option<BlockHash>) -> RpcResult<Balance>;
+}
